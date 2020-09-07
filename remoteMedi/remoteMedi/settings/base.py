405581f522b33bc5ac864bbfa74afe2924b10202
ilsong963 <50066666+ilsong963@users.noreply.github.com>
2020-09-07 15:29:52 +0900

    카카오톡 로그아웃 구현 완료

--- remoteMedi/remoteMedi/settings/base.py
+++ remoteMedi/remoteMedi/settings/base.py
@@ -24,6 +24,7 @@ load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 KAKAO_API = os.getenv('KAKAO_API')
+KAKAO_SECRET = os.getenv('KAKAO_SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 print(KAKAO_API)
 
