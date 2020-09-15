b585ff5be61f9f7d81f7de86269c7bdac04aa356
ilsong963 <50066666+ilsong963@users.noreply.github.com>
2020-09-15 23:13:26 +0900

    google room number

--- remoteMedi/authenticate/urls.py
+++ remoteMedi/authenticate/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from .views import login
+from .views import createroom
+from .views import joinroom
+
 urlpatterns = [
     path('login/', login),
+    path('api/createroom', createroom),
+    path('api/joinroom', joinroom),
 ]
--- remoteMedi/authenticate/views.py
+++ remoteMedi/authenticate/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+import json
+import hashlib 
+from rest_framework import status
+
 
 # Create your views here.
 
@@ -13,3 +17,40 @@ def login(request):
 
     # 리턴
     return Response(status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+def createroom(request):
+    res = json.loads(request.body)
+    print(res['identity'])
+    
+    enc = hashlib.md5()
+    enc.update(res['identity'].encode('utf-8'))
+    encText = enc.hexdigest()
+    print(encText)
+
+    
+    """
+    #대충 DB관련 중복검사 및 insert 
+    #
+    #
+    #
+    """ 
+    
+    return Response(status=status.HTTP_200_OK)
+    
+@api_view(['POST'])
+def joinroom(request):
+    res = json.loads(request.body)
+    print(res['identity'])    
+    
+    
+    """
+    #대충 DB관련 데이터 검사 select
+    #
+    #
+    #
+    """        
+    roomnumber = 123
+    return Response(roomnumber,status=status.HTTP_200_OK)
+    
