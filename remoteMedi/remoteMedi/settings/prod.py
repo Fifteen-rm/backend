07f1533108edcdac08b4364a23c3f13ec5ce3676
key kim <keyhyuk.kim@gmail.com>
2020-11-26 00:34:28 +0900

    Add cors

--- remoteMedi/remoteMedi/settings/prod.py
+++ remoteMedi/remoteMedi/settings/prod.py
@@ -1,8 +1,21 @@
 from .base import *
 import os
 
-CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_CREDENTIALS = True
+ALLOWED_HOSTS = [    
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'https://keykim.ddns.net',
+    'https://keykim.me'
+]
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'https://keykim.ddns.net',
+    'https://keykim.me'
+)
+
+
 DEBUG = True
 HOST = os.getenv('DEBUG_HOST')
 INSTALLED_APPS += ['django_s3_storage']
@@ -12,7 +25,7 @@ AWS_S3_BUCKET_NAME_STATIC = S3_BUCKET
 STATIC_URL = "https://%s.s3.amazonaws.com/" % S3_BUCKET
 
 #ALLOWED_HOSTS = ["127.0.0.1", "kauth.kakao.com", "vjbw52tg5f.execute-api.us-east-2.amazonaws.com"]
-ALLOWED_HOSTS = ["*"]
+
 
 if os.getenv("GITHUB_WORKFLOW"):
    DATABASES = {
