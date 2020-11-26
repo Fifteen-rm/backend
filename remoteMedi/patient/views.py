acce623c4c4d1e827dad04243af0f62f07e5016b
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2020-11-26 23:21:07 +0900

    Logger description_str

--- remoteMedi/patient/views.py
+++ remoteMedi/patient/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from django.http import JsonResponse, HttpResponse
 from treatment.models import Description
+from treatment.models import WaitingRoom
 from django.conf import settings
 from rest_framework.response import Response
 from django.core import serializers
@@ -9,6 +10,7 @@ from .serializers import PatientSerializer, DescriptionSerializer
 import json
 import requests
 import logging
+import hashlib
 # Create your views here.
 
 
@@ -18,6 +20,8 @@ logger = logging.getLogger(__name__)
 @api_view(['POST'])
 def description(request):
     
+    res = json.loads(request.body) # Patient 로 이름
+    # Authorization에 토큰을 넣어주세요  Bearer manm_8uKTUNVw-b_1aTRkZZsbLE_OVox4J_Y8Ao9dJcAAAF10YgdEw 요롷게
     Authorization = request.headers.get('Authorization')
 
     #인증 실패시
@@ -33,6 +37,7 @@ def description(request):
     descriptions_str += "\n환자  성명 : " + str(descriptions.patient.name)
     descriptions_str += "\n증상 : " + str(descriptions.patient_say)
     descriptions_str += "\n진단 내용 : " + str(descriptions.doctor_say)
+    logger.info(descriptions_str)
     logger.error(descriptions_str)
 
 
@@ -59,6 +64,13 @@ def description(request):
         }
     send_url = "https://kapi.kakao.com/v2/api/talk/memo/default/send"
     result = requests.post(url=send_url, headers = headers, data = data)
+    
+    enc = hashlib.md5()
+    enc.update(res['Patient'].encode('utf-8'))
+    encText = enc.hexdigest()
+
+
+    WaitingRoom.objects.filter(room_number=encText).delete()
 
     return Response(result)
 
@@ -75,7 +87,6 @@ def view_description(request):
     
     try:
         descriptions = Description.objects.all().order_by('-created_at')
-
     except:
         return JsonResponse(status = 404)
 
@@ -83,4 +94,5 @@ def view_description(request):
     ser = DescriptionSerializer(data=descriptions, many=True)
     ser.is_valid()
     
+
     return JsonResponse(ser.data,status=200, safe=False)
